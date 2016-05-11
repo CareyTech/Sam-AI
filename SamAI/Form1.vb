f47410c149df19e0f387dc2e9af13015fc5d6215
wfltaylor <wfltaylor@gmail.com>
2016-05-11 13:54:03 +1000

    First

---
+++ SamAI/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Public Class Form1
+    Dim privid As String = ""
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        savestring(TextBox1.Text, privid)
+        ListBox1.Items.Add("Thanks")
+    End Sub
+    Sub savestring(data As String, key As String)
+        Dim path As String = "C:\Users\william.taylor\Documents\AI\" + key + ".txt"
+
+        ' Create or overwrite the file.
+        Dim fs As FileStream = File.Create(path)
+
+        ' Add text to the file.
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes(data)
+        fs.Write(info, 0, info.Length)
+        fs.Close()
+    End Sub
+    Function readdata(key As String)
+        Dim fileReader As String
+        Try
+            fileReader = My.Computer.FileSystem.ReadAllText("C:\Users\william.taylor\Documents\AI\" + key + ".txt")
+        Catch ex As Exception
+            fileReader = "I don't know what you mean. Teach me!"
+        End Try
+
+        Return fileReader
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim numid As Integer = getnum()
+        privid = numid
+        ListBox1.Items.Add(readdata(numid.ToString))
+    End Sub
+    Function getnum()
+        Dim hi As Integer = 0
+        If TextBox1.Text.Contains("hi") Then
+            hi = hi + 42
+        End If
+        'hi
+        If TextBox1.Text.Contains("hello") Then
+            hi = hi + 42
+        End If
+        If TextBox1.Text.Contains("name") Then
+            hi = hi + 78
+        End If
+        If TextBox1.Text.Contains("Will") Then
+            hi = hi + 92
+        End If
+        If TextBox1.Text.Contains("time") Then
+            hi = hi + 145
+        End If
+        Return hi
+    End Function
+End Class
---
+++ SamAI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SamAI")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SamAI")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("311cb58d-0dec-4dd6-9c60-00d1711aeb56")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
